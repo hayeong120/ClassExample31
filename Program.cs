fd595e0d80f377c21105e3974b2f43fc40f8af42
hayeong120 <jbg01200249@gmail.com>
2025-06-13 10:08:59 +0900

    상속 관계에서의 생성자

--- Program.cs
+++ Program.cs
@@ -10,19 +10,45 @@ namespace ClassExample31
     {
         class Parent
         {
-            public Parent() { Console.WriteLine("부모 생성자"); }
+            public void CountParent()
+            {
+                Parent.counter++;
+            }
+            public Parent() { Console.WriteLine("Parent()"); }
+            public Parent(string name) { Console.WriteLine("Parent(string name)"); }
+
+            public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+            public Parent(double param) { Console.WriteLine("Parent(int double)"); }
         }
 
-        public class Child : Parent
+        class Child : Parent
         {
-            public Child()
+            public void CountChild()
             {
+                Child.counter++;
+            }
+            public Child() : base("child")
+            {
+                Console.WriteLine("Child()");
+            }
+            public Child(string name) : base(name)
+            {
+                Console.WriteLine("Child(string name)");
+            }
+            public Child(int param) : base(param)
+            {
+                Console.WriteLine("Child(double parsm)");
             }
         }
 
         static void Main(string[] args)
         {
-            Child child = new Child();
+            Child childA = new Child("abc");
+            Child childB = new Child(3L);
+
+            parent.CountParent();
+            child.CoumnChild();
+            
         }
     }
 }
