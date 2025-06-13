debeb2295944ed469a588505776174b6b8d6334d
hayeong120 <jbg01200249@gmail.com>
2025-06-13 09:50:45 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassExample31
+{
+    static class Program
+    {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("부모 생성자"); }
+        }
+
+        public class Child : Parent
+        {
+            public Child()
+            {
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+        }
+    }
+}
